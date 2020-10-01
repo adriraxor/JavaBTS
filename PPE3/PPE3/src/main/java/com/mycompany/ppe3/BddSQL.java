07886fafea2a36976cf6b9783852900e90c56deb
adriraxor <figueres.adrien@gmail.com>
2020-10-01 08:07:35 +0200

    Add files via upload

---
+++ PPE3/PPE3/src/main/java/com/mycompany/ppe3/BddSQL.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ */
+package com.mycompany.ppe3;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author FIGUERES Adrien
+ */
+public class BddSQL {
+    
+    
+    Connection connexion;
+    
+    public void connexionBdd(){
+        
+        Formulaire form = new Formulaire();
+        //Information(s) BDD
+        String url = "jdbc:mysql://localhost/ppe3_logicielbdd_off"; 
+        String logUser = "root";
+        String logPass = "toor";
+         
+        //Connexion BDD
+        try {
+            this.connexion = (Connection) DriverManager.getConnection(url, logUser, logPass);
+            System.out.println("Connexion Enabled with the DataBase");
+            System.out.println("Interaction Enabled with the Database");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(BddSQL.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Connexion Failed with the DataBase");
+            System.out.println("Interaction Failed with the DataBase");
+            
+        }
+       
+        
+    } 
+}
